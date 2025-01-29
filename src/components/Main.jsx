e5d1016d8e7e37f4bc64d922ba6e7ab0598c8825
lisadelladucata <lisa.delladucata@gmail.com>
2025-01-29 10:10:17 +0100

    feat: add-main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,130 @@
 const content = '--> content goes here <--';
 
 export default function Main(){
-    return <main>{content}</main>
+    return <main>
+        <section className="first-section">
+            <div className="container">
+                {content}
+            </div>
+        </section>
+        <section className="second-section">
+            <div className="container">
+                <div>
+                    <img src="./img/buy-comics-digital-comics.png" alt="icon1" />
+                    <a href="#">DIGITAL COMICS</a>
+                </div>
+                <div>
+                    <img src="./img/buy-comics-merchandise.png" alt="icon2" />
+                    <a href="#">DC MERCHANDISE</a>
+                </div>
+                <div>
+                    <img src="./img/buy-comics-subscriptions.png" alt="icon3" />
+                    <a href="#">SUBSCRIPTION</a>
+                </div>
+                <div>
+                    <img src="./img/buy-comics-shop-locator.png" alt="icon4" />
+                    <a href="#">COMIC SHOP LOCATOR</a>
+                </div>
+                <div>
+                    <img src="./img/buy-dc-power-visa.svg" alt="icon5" />
+                    <a href="#">DC POWER VISA</a>
+                </div>
+            </div>
+        </section>
+        <section className="third-section">
+            <div className="container row">
+                <div>
+                    <h2>DC COMICS</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+                        <li>
+                            <a href="#">Games</a>
+                        </li>
+                        <li>
+                            <a href="#">Videos</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>SHOP</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Shop DC</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop DC Collectibles</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>DC</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Terms of use</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy policy</a>
+                        </li>
+                        <li>
+                            <a href="#">Add Choices</a>
+                        </li>
+                        <li>
+                            <a href="#">Advertiding</a>
+                        </li>
+                        <li>
+                            <a href="#">Jobs</a>
+                        </li>
+                        <li>
+                            <a href="#">Subscriptions</a>
+                        </li>
+                        <li>
+                            <a href="#">Talent Workshops</a>
+                        </li>
+                        <li>
+                            <a href="#">Ratings</a>
+                        </li>
+                        <li>
+                            <a href="#">Shop Help</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Sites</h2>
+                    <ul>
+                        <li>
+                            <a href="#">DC</a>
+                        </li>
+                        <li>
+                            <a href="#">MAD Magazine</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Kids</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Universe</a>
+                        </li>
+                        <li>
+                            <a href="#">DC Power Visa</a>
+                        </li>
+                    </ul>
+                </div>
+            <img src="./img/dc-logo-bg.png" alt="" />
+
+            </div>
+        </section>
+    </main>
 }
