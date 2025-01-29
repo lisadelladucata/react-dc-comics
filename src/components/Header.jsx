5db4ab3176a283d3e122f78a00795560b7e9a2f5
lisadelladucata <lisa.delladucata@gmail.com>
2025-01-29 01:38:54 +0100

    feat:add-header

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+export default function Header(){
+    return <header className="container">
+        <img src="./img/dc-logo.png" alt="logo-header" />
+        <nav className="main-nav">
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </nav>
+        
+    </header>
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+const content = '--> content goes here <--';
+
+export default function Main(){
+    return <main>{content}</main>
+}
